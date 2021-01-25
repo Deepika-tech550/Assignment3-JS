ae2aa785b303f7475569364c47ef3563de129a6e
Deepika-tech550 <75797675+Deepika-tech550@users.noreply.github.com>
2021-01-25 09:11:35 +0530

    Update virtual_talking.py

--- virtual_talking.py
+++ virtual_talking.py
@@ -1,10 +1,20 @@
-import pyttsx3
-def fun(msg):
-    friend=pyttsx3.init()
-    friend.say(msg)
-    friend.runAndWait()
-    
-    friend.stop()
-    print("Successfulluy Speaked!!!!!")
-text=input("Enter The Text To Speak")    
-fun(text)
+
+
+
+
+
+
+
+
+
+
+2.
+
+var age=7
+if(age<18){
+  alert("Not legal age to drive")
+  
+}
+else{ 
+  alert("Drive safe")
+}
